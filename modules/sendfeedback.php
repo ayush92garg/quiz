b420dcab06c88b8707a6ddf8d62cdeeb31782606
ayush92garg <think.ayushgarg@gmail.com>
2016-04-03 15:56:39 +0530

    file path fix

--- modules/sendfeedback.php
+++ modules/sendfeedback.php
@@ -1,7 +1,7 @@
 <?php 
 	$email = $_GET["email"];
 	$message = $_GET["content"];
-	$feedback_directory_path = "../feedback";
+	$feedback_directory_path = "../feedbacks/";
 	$fb_array = array ("email"=>$email,"message"=>$message);
 
 	file_put_contents($feedback_directory_path.$email.strtotime("now")."json", json_encode($fb_array));
